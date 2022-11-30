75e7a9e5ead419c8507a6bbda69e284ede5e4640
Nadezhda <nbykova79@gmail.com>
2022-11-30 18:42:21 +0300

    Create class User

---
+++ src/main/java/re/gb/oseminar/data/data/User.java
@@ -0,0 +1,43 @@
+package re.gb.oseminar.data.data;
+
+public abstract class User {
+    private String firstName;
+    private String secondName;
+    private String patronymic;
+
+    public User(String firstName, String secondName, String patronymic) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.patronymic = patronymic;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    @Override
+    public String toString() {
+        return "Student: " +
+                secondName +" "+ firstName  + " " + patronymic;
+    }
+}
