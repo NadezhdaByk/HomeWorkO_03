a615f7ed6145fe50930a86f3a55e9102ccbdd4c4
Nadezhda <nbykova79@gmail.com>
2022-11-30 18:40:04 +0300

    Create class Student

---
+++ src/main/java/re/gb/oseminar/data/data/Student.java
@@ -0,0 +1,23 @@
+package re.gb.oseminar.data.data;
+
+public class Student extends User {
+
+    private Long studentID;
+
+    public Student(String firstName, String lastName, String patronymic, Long studentID) {
+        super(firstName, lastName, patronymic);
+        this.studentID = studentID;
+    }
+
+    public Long getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(Long studentID) {
+        this.studentID = studentID;
+    }
+
+
+}
+
+
